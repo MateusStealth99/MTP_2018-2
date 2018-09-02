f523ef392d99023eca929d6f8d2496619ab6f547
MateusStealth99 <42480176+MateusStealth99@users.noreply.github.com>
2018-09-02 20:48:33 -0300

    Add files via upload

---
+++ p0.c
@@ -0,0 +1,22 @@
+/*P0.c*/
+/*meteus da silva fernandes*/
+/*11811ETE013*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	int i=0;
+	if (argc>1)
+	{
+	if(argc>2){
+		for ( i = 0; i < argv[2][0]-'0'; ++i){
+			printf("Valeu,%s\n",argv[1]);
+		}
+	}
+	else {
+		printf("Saudacoes, %s\n",argv[1] );
+	}
+		
+	}else
+	printf("ola\n");
+	return 0;
+}

b223238872ea83aa5f82d0851cc2bdaffc88ee94
MateusStealth99 <42480176+MateusStealth99@users.noreply.github.com>
2018-09-12 21:13:35 -0300

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,18 @@
+/*p1.c*/
+/*mateus da silva fernandes*/
+/*11811ETE013*/
+
+
+#include <stdio.h>
+
+int main(){
+	char numero[250];
+	scanf("%s",numero);
+	int estado=0,n=0,i;
+	for(i=0;numero[i];i++){
+		n=numero[i]-48;
+		estado=estado==0?n:estado==1?n==0?2:0:n==1?2:1;
+		}
+		 printf("%s %s",numero,estado==0?"e multiplo de 3":"nao e");
+	return 0;
+	}

dec5c80439ad33b7e55d78ee667b6a770998b922
MateusStealth99 <42480176+MateusStealth99@users.noreply.github.com>
2018-10-17 22:01:38 -0300

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,19 @@
+/*p3.c*/
+/*mateus da silva fernandes*/
+/*11811ETE013*/
+#include <stdio.h>
+void integerValueOf(char *str,int *n){
+		if(*str){
+			if(*str>='0'&&*str<='9')
+				*n=*n*10+((*str-'0'));
+	    integerValueOf((str+1), n);
+    }
+}
+int main(){
+	char str[256]="";
+	int n;
+	scanf("%s",str);
+	integerValueOf(&str[0], &n);
+	printf("%d",n);
+	return 0;
+}
